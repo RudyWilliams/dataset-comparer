05e4c604fcf893f0f59b98ebd478dd76b7b49a89
Rudy Williams <rudysw05@knights.ucf.edu>
2020-05-22 16:56:53 -0400

    turns to class defintion

--- comparer.py
+++ comparer.py
@@ -1,35 +1,63 @@
 import os
 import pandas as pd
 
-datapath1 = os.path.join(
-    "tests", "data", "no-mismatches", "one2one-A1.csv"
-)  # config (dev vs production like with snapshot software)
-datapath2 = os.path.join("tests", "data", "no-mismatches", "one2one-A2.csv")  # config
 
-df1 = pd.read_csv(datapath1, parse_dates=["intake_dt", "exit_dt"])  # config parsedates
-df2 = pd.read_csv(datapath2, parse_dates=["intake_dt", "exit_dt"])  # config parsedates
+class Comparer:
+    def __init__(self, path1, path2):
+        self.path1 = path1
+        self.path2 = path2
+        self.df1 = None
+        self.df2 = None
+        # self.handcheck = None
 
-match_on = ["last_name", "first_name"]  # config
+    def set_dataframes(self, **kwargs):
+        self.df1 = pd.read_csv(self.path1, **kwargs)
+        self.df2 = pd.read_csv(self.path2, **kwargs)
+        return self
 
-df1_groups = dict(tuple(df1.groupby(match_on)))
-df2_groups = dict(tuple(df2.groupby(match_on)))
+    def compare_dataframes(self, match_on, compare_on):
+        n_compare_columns = len(compare_on)
+        same_columns = [f"_same_{c}" for c in compare_on]
+        df1_groups = tuple(self.df1.groupby(match_on))
+        df2_groups_dict = dict(tuple(self.df2.groupby(match_on)))
 
-handcheck = []
+        handcheck = []
 
-for name, data in df1_groups.items():
-    compare = df2_groups.get(name)
-    if compare.empty:
-        # when a potential match cannot be found
-        handcheck.append(data)
-        continue
+        for identifier, data in df1_groups:
+            # only need to check a subset of df2
+            compare = df2_groups_dict.get(identifier, None)
+            if compare is None:
+                print(f"{identifier} could not be found in other data set.")
+                handcheck.extend(list(data.itertuples()))
+                continue
 
-    for obs in data.itertuples():
-        # this will need updating to generalize and use configurations
-        compare["same_intake"] = compare["intake_dt"] == obs.intake_dt
-        compare["same_exit"] = compare["exit_dt"] == obs.exit_dt
-        compare["same_reason"] = compare["release_reason"] == obs.release_reason
-        compare["match_sum"] = compare[["same_intake", "same_exit", "same_reason"]].sum(
-            axis=1
-        )
-        if 3 in compare["match_sum"].values:  # 3 will have to be dynamic to generalize
-            print(f"{name[1]} {name[0]}'s record has match in other dataset")
+            for obs in data.itertuples():
+
+                obs_dict = obs._asdict()
+                for column, same_column in zip(compare_on, same_columns):
+                    compare[same_column] = compare[column] == obs_dict[column]
+                compare["_match_sum"] = compare[same_columns].sum(axis=1)
+
+                if n_compare_columns in compare["_match_sum"].values:
+                    print(f"{identifier} has matching records in data sets.")
+                else:
+                    print(f"{identifier} did NOT have matching record in data sets.")
+                    handcheck.append(obs)
+
+        return handcheck
+
+
+if __name__ == "__main__":
+
+    datapath1 = os.path.join(
+        "tests", "data", "unmatchable", "one2one-A1.csv"
+    )  # config (dev vs production like with snapshot software)
+    datapath2 = os.path.join("tests", "data", "unmatchable", "one2one-A2.csv")  # config
+    c = Comparer(datapath1, datapath2)
+    c.set_dataframes(parse_dates=["intake_dt", "exit_dt"])
+    handcheck = c.compare_dataframes(
+        match_on=["last_name", "first_name"],
+        compare_on=["intake_dt", "exit_dt", "release_reason"],
+    )
+
+    print(pd.DataFrame(handcheck))
