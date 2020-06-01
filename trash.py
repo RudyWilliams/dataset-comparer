bfd478b340e0c9df58dc4c762b6de80abe7f38c3
Rudy Williams <rudysw05@knights.ucf.edu>
2020-06-01 16:06:11 -0400

    trash.py shows a working use case

--- trash.py
+++ trash.py
@@ -1,13 +1,22 @@
 from pathlib import Path
 import pandas as pd
+from juxta.pose.comparer import Comparer
 
-path = (
+path1 = (
+    Path("juxta") / "tests" / "data" / "mismatches" / "one-to-one" / "one_to_one-A1.csv"
+)
+path2 = (
     Path("juxta")
     / "tests"
     / "data"
-    / "no-mismatches"
+    / "mismatches"
     / "one-to-one"
-    / "one_to_one-A1.csv"
+    / "one_to_one-A2-shuffled.csv"
 )
-df = pd.read_csv(path)
-print(df)
+
+
+comparer = Comparer(path1, path2)
+comparer.set_dataframes(parse_dates=["intake_dt", "exit_dt"]).compare_dataframes(
+    group_on=["last_name", "first_name"],
+    compare_on=["intake_dt", "exit_dt", "release_reason"],
+).results_to_df().results_to_csv("set_intermediate_df_test.csv", index=False)
