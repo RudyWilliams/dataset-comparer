d188e7e1062d4c4b66edb144ad5ee2a9148e2ba2
Rudy Williams <rudysw05@knights.ucf.edu>
2020-06-01 12:22:12 -0400

    updates so truths are fixtures

--- conftest.py
+++ conftest.py
@@ -1,7 +1,14 @@
 from collections import namedtuple
 from pathlib import Path
+import pandas as pd
 import pytest
 from juxta.pose.comparer import Comparer
+
+
+Pandas = namedtuple(
+    "Pandas",
+    ["Index", "last_name", "first_name", "intake_dt", "exit_dt", "release_reason"],
+)
 
 
 @pytest.fixture
@@ -180,6 +187,178 @@ def shuffled_many_to_many_unmatchable_obj():
     comparer = Comparer(path1=d1_path, path2=d2_path)
     comparer.set_dataframes(parse_dates=["intake_dt", "exit_dt"])
     return comparer
+
+
+@pytest.fixture
+def namedtuple_list_1():
+    return [
+        Pandas(
+            Index=3,
+            last_name="Smith",
+            first_name="Jonathan",
+            intake_dt=pd.Timestamp("2018-01-08"),
+            exit_dt=pd.Timestamp("2019-10-24"),
+            release_reason="c",
+        ),
+        Pandas(
+            Index=7,
+            last_name="Landry",
+            first_name="Kristina",
+            intake_dt=pd.Timestamp("2018-06-18"),
+            exit_dt=pd.Timestamp("2019-04-01"),
+            release_reason="a",
+        ),
+    ]
+
+
+@pytest.fixture
+def namedtuple_list_2():
+    return [
+        Pandas(
+            Index=0,
+            last_name="Houston",
+            first_name="Dennis",
+            intake_dt=pd.Timestamp("2018-01-20"),
+            exit_dt=pd.Timestamp("2019-01-12"),
+            release_reason="c",
+        ),
+        Pandas(
+            Index=6,
+            last_name="Perez",
+            first_name="Denise",
+            intake_dt=pd.Timestamp("2019-06-18"),
+            exit_dt=pd.Timestamp("2019-08-03"),
+            release_reason="c",
+        ),
+        Pandas(
+            Index=9,
+            last_name="Johnson",
+            first_name="Anthony",
+            intake_dt=pd.Timestamp("2019-07-18"),
+            exit_dt=pd.Timestamp("2019-03-31"),
+            release_reason="c",
+        ),
+    ]
+
+
+@pytest.fixture
+def namedtuple_list_3():
+    return [
+        Pandas(
+            Index=0,
+            last_name="Houston",
+            first_name="Dennis",
+            intake_dt=pd.Timestamp("2018-01-20"),
+            exit_dt=pd.Timestamp("2019-01-12"),
+            release_reason="c",
+        ),
+        Pandas(
+            Index=6,
+            last_name="Perez",
+            first_name="Denise",
+            intake_dt=pd.Timestamp("2019-06-18"),
+            exit_dt=pd.Timestamp("2019-08-03"),
+            release_reason="c",
+        ),
+    ]
+
+
+@pytest.fixture
+def namedtuple_list_4():
+    return [
+        Pandas(
+            Index=3,
+            last_name="Smith",
+            first_name="Jonathan",
+            intake_dt=pd.Timestamp("2018-01-08"),
+            exit_dt=pd.Timestamp("2019-10-24"),
+            release_reason="c",
+        ),
+        Pandas(
+            Index=10,
+            last_name="Smith",
+            first_name="Jonathan",
+            intake_dt=pd.Timestamp("2018-08-19"),
+            exit_dt=pd.Timestamp("2019-05-10"),
+            release_reason="b",
+        ),
+        Pandas(
+            Index=11,
+            last_name="Smith",
+            first_name="Jonathan",
+            intake_dt=pd.Timestamp("2018-04-04"),
+            exit_dt=pd.Timestamp("2019-12-14"),
+            release_reason="b",
+        ),
+        Pandas(
+            Index=7,
+            last_name="Landry",
+            first_name="Kristina",
+            intake_dt=pd.Timestamp("2018-06-18"),
+            exit_dt=pd.Timestamp("2019-04-01"),
+            release_reason="a",
+        ),
+    ]
+
+
+@pytest.fixture
+def namedtuple_list_5():
+    return [
+        Pandas(
+            Index=3,
+            last_name="Smith",
+            first_name="Jonathan",
+            intake_dt=pd.Timestamp("2018-01-08"),
+            exit_dt=pd.Timestamp("2019-10-24"),
+            release_reason="c",
+        ),
+        Pandas(
+            Index=10,
+            last_name="Smith",
+            first_name="Jonathan",
+            intake_dt=pd.Timestamp("2018-08-19"),
+            exit_dt=pd.Timestamp("2019-05-10"),
+            release_reason="b",
+        ),
+    ]
+
+
+@pytest.fixture
+def namedtuple_list_6():
+    return [
+        Pandas(
+            Index=0,
+            last_name="Houston",
+            first_name="Dennis",
+            intake_dt=pd.Timestamp("2018-01-20"),
+            exit_dt=pd.Timestamp("2019-01-12"),
+            release_reason="c",
+        ),
+        Pandas(
+            Index=8,
+            last_name="Valenzuela",
+            first_name="Clifford",
+            intake_dt=pd.Timestamp("2018-11-24"),
+            exit_dt=pd.Timestamp("2019-05-15"),
+            release_reason="b",
+        ),
+        Pandas(
+            Index=10,
+            last_name="Valenzuela",
+            first_name="Clifford",
+            intake_dt=pd.Timestamp("2018-08-19"),
+            exit_dt=pd.Timestamp("2019-10-16"),
+            release_reason="b",
+        ),
+        Pandas(
+            Index=11,
+            last_name="Valenzuela",
+            first_name="Clifford",
+            intake_dt=pd.Timestamp("2018-04-04"),
+            exit_dt=pd.Timestamp("2019-05-10"),
+            release_reason="b",
+        ),
+    ]
 
 
 if __name__ == "__main__":
