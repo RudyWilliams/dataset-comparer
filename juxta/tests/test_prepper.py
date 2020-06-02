11bba2f1d987508612fa2fca4b7b2997b85fd397
Rudy Williams <rudysw05@knights.ucf.edu>
2020-06-02 12:18:47 -0400

    adds mapping testing

--- juxta/tests/test_prepper.py
+++ juxta/tests/test_prepper.py
@@ -6,8 +6,6 @@ def test_keep_relevant_columns_same_naming_infer(input_df_1, input_df_2):
     prepper = Prepper(dataset1=input_df_1, dataset2=input_df_2)
     keep_columns = ["last_name", "first_name", "intake_dt", "exit_dt", "release_reason"]
     prepper.keep_relevant_columns(ds1_columns=keep_columns)
-    print(prepper.df1.columns)
-    print(prepper.df2.columns)
     assert (prepper.df1.columns == keep_columns).all()
     assert (prepper.df2.columns == keep_columns).all()
 
@@ -16,8 +14,6 @@ def test_keep_relevant_columns_same_naming_explicit(input_df_1, input_df_2):
     prepper = Prepper(dataset1=input_df_1, dataset2=input_df_2)
     keep_columns = ["last_name", "first_name", "intake_dt", "exit_dt", "release_reason"]
     prepper.keep_relevant_columns(ds1_columns=keep_columns, ds2_columns=keep_columns)
-    print(prepper.df1.columns)
-    print(prepper.df2.columns)
     assert (prepper.df1.columns == keep_columns).all()
     assert (prepper.df2.columns == keep_columns).all()
 
@@ -31,13 +27,31 @@ def test_keep_relevant_columns_diff_naming_explicit(input_df_1, input_df_3):
         "exit_dt",
         "release_reason",
     ]
-    print(prepper.df2.columns)
     keep_columns2 = ["lastname", "firstname", "intake_date", "exit_date", "discharge"]
     prepper.keep_relevant_columns(ds1_columns=keep_columns1, ds2_columns=keep_columns2)
-    print(prepper.df1.columns)
-    print(prepper.df2.columns)
     assert (prepper.df1.columns == keep_columns1).all()
     assert (prepper.df2.columns == keep_columns2).all()
 
 
 # tests for making column names consistent
+def test_column_name_mapping(input_df_1, input_df_3):
+    prepper = Prepper(dataset1=input_df_1, dataset2=input_df_3)
+    keep_columns1 = [
+        "last_name",
+        "first_name",
+        "intake_dt",
+        "exit_dt",
+        "release_reason",
+    ]
+    keep_columns2 = ["lastname", "firstname", "intake_date", "exit_date", "discharge"]
+    prepper.keep_relevant_columns(ds1_columns=keep_columns1, ds2_columns=keep_columns2)
+    prepper.map_to_ds1_column_names(
+        mapper={
+            "lastname": "last_name",
+            "firstname": "first_name",
+            "intake_date": "intake_dt",
+            "exit_date": "exit_dt",
+            "discharge": "release_reason",
+        }
+    )
+    assert (prepper.df1.columns == prepper.df2.columns).all()
